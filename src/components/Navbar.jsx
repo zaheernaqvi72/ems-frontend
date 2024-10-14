775b0f9e08047641b5002c0427b9357c20bc52b2
Sayed Zaheer Abass <144617512+zaheernaqvi72@users.noreply.github.com>
2024-10-15 05:12:29 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         >
           <img
             className="h-10 rounded-lg"
-            src="../assets/logo.png"
+            src="src/assets/logo.png"
             alt=""
           />
           <span className="text-2xl p-2">Employee Management</span>
